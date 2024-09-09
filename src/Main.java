c422861f31e510aa425f140341f5ce38702ba8b4
Hustini <ejdersalih02@gmail.com>
2024-09-09 08:17:14 +0200

    added a screen

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("welcome to TicTacToe");
+        // Frame size
+        int boardWidth = 600;
+        int boardHeight = 600;
+
+        // Frame
+        JFrame frame = new JFrame("TicTacToe");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
