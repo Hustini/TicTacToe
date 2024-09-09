4b733efc94337a9a4810b8680c2c361d8b0ef6da
Hustini <ejdersalih02@gmail.com>
2024-09-09 09:22:36 +0200

    now visible board

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,8 +2,22 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TicTacToe extends JPanel {
+    private class Tile {
+        int x;
+        int y;
+
+        Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     int boardWidth;
     int boardHeight;
+    int tileSize = 200;
+
+    String[][] board = new String[3][3];
+    Tile tile = new Tile(0, 0);
 
     TicTacToe(int boardHeight, int boardWidth) {
         this.boardHeight = boardHeight;
@@ -11,5 +25,19 @@ public class TicTacToe extends JPanel {
         setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
         setBackground(Color.BLACK);
         setFocusable(true);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                g.setColor(Color.GRAY);
+                g.fill3DRect(tile.x * tileSize + (j * 200), tile.y * tileSize + (i * 200), tileSize, tileSize, true);
+            }
+        }
     }
 }
