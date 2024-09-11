fa24eedb2a7fb6c418e7a92437ccdc6a379b8f0a
Hustini <ejdersalih02@gmail.com>
2024-09-11 09:30:51 +0200

    deleted unused import

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.Arrays;
 
 public class TicTacToe extends JPanel implements MouseListener {
     private class Tile {
